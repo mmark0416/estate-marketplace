3c49d0d323a71141063e868f5989ef83b4008403
Majoros Jozsef Mark <mmark0416@gmail.com>
2023-10-26 12:30:42 +0200

    create update listing api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -9,10 +9,26 @@ export const createListing = async (req, res) => {
 
 export const deleteListing = async (req, res) => {
   const listing = await Listing.findById(req.params.id);
-  if(!listing) throw new PageNotFoundError("Listing not found")
-  
-  if(req.user.id !== listing.userRef) throw new UnauthorizedError("You can only delete your own listing")
-  
-  await Listing.findByIdAndDelete(req.params.id)
+  if (!listing) throw new PageNotFoundError("Listing not found");
+
+  if (req.user.id !== listing.userRef)
+    throw new UnauthorizedError("You can only delete your own listing");
+
+  await Listing.findByIdAndDelete(req.params.id);
   return res.status(201).json("Delete successfully");
+};
+
+export const updateListing = async (req, res) => {
+  const listing = await Listing.findById(req.params.id);
+
+  if (!listing) throw new PageNotFoundError("Listing not found");
+  if (req.user.id !== listing.userRef)
+    throw new UnauthorizedError("You can only update your own listing");
+
+  const updatedListing = await Listing.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {new: true}
+  )
+  res.status(200).json(updatedListing)
 };
